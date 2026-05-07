d7428de80b15ba1be6b4153e11f5d9839d848223
er-khanliyakat <liyakatkhan@ilssrinagar.com>
2026-05-07 20:36:51 +0530

    m1

---
+++ app.py
@@ -0,0 +1,6 @@
+def login(password):
+    if password == "1234":
+        return "Login successful"
+    else:
+        return "Login failed"
+    
---
+++ test_app.py
@@ -0,0 +1,6 @@
+from app import login
+def test_login_success():
+    assert login("1234") == "Login successful"
+
+def test_login_failure():
+    assert login("wrongpassword") == "Login failed"
